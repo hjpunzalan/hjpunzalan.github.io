4e75ab475dcc34dfcce5ae6b41ea3a361c3bf4b4
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-06-11 16:15:32 +0800

    Updates

---
+++ precache-manifest.0568d6883d1023be621e0952d433fd36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4750d16ed035bca1204aa635e02986c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dead600525aec6f996c",
+    "url": "/static/css/main.c21e25dc.chunk.css"
+  },
+  {
+    "revision": "b4ed2f709007b217f2bb",
+    "url": "/static/js/2.6c6cf81f.chunk.js"
+  },
+  {
+    "revision": "4dead600525aec6f996c",
+    "url": "/static/js/main.61921e51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87fa4c272e4711bb7373d96203202e20",
+    "url": "/static/media/developer.87fa4c27.gif"
+  },
+  {
+    "revision": "c140d764d2c44aa557d7b9cdb66452f9",
+    "url": "/static/media/logo.c140d764.jpg"
+  }
+]);
