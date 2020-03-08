33b9d0bcbd561957ffa9764f5c6be4e192a431d1
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-08 23:35:35 +0800

    added hero component and style

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Hero.module.scss";
+
+const Hero = () => {
+	return (
+		<div className={classes.container}>
+			<h1>Jonathan Punzalan</h1>
+		</div>
+	);
+};
+
+export default Hero;
