f114a91599c036fe3dc1fc3edba298bdffa840ab
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-09 13:38:42 +0800

    about me section - image added and font chosen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
 	return (
 		<div className="App">
 			<header>
 				<Hero />
+				<About />
 			</header>
 		</div>
 	);
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import classes from "./About.module.scss";
+import img from "../../images/myself.jpg";
 
 const About = () => {
-	return <div className={classes.container}></div>;
+	return (
+		<div className={classes.container}>
+			<img className={classes.photo} src={img} alt="myself" />
+			<div className={classes.about}>
+				<h2>
+					Jonathan Punzalan is a full-stack developer. One who is continuously
+					driven by passion to build problem-solving applications.
+				</h2>
+				<p>I am a self-taught developer.</p>
+			</div>
+		</div>
+	);
 };
 
 export default About;
