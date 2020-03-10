5c5bb1095efa2567b86f21db3e052b47803ec640
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-10 20:31:12 +0800

    Projects component initialised

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<header>
 				<Hero />
 				<About />
+				<Projects />
 			</header>
 		</div>
 	);
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./Projects.module.scss";
+
+export class Projects extends Component {
+	render() {
+		return <div className={classes.container}></div>;
+	}
+}
+
+export default Projects;
