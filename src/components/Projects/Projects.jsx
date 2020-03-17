23e125ae5bd181203bf4ffcd3dd7e68bc7a114cb
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-17 23:35:40 +0800

    project section added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import classes from "./Projects.module.scss";
+import budgety1 from "../../images/projects/budgety-1.png";
 
 export class Projects extends Component {
 	render() {
 		return (
 			<div className={classes.container}>
 				<h1>Projects</h1>
+				<div className={classes.project}>
+					<img src={budgety1} alt="budgety-1" />
+					<div className={classes.desc}>
+						<h2>Budgety App</h2>
+						<p>This is the budgety app</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
