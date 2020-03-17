75eaa8eb69ca643d09664b60d9f8168ff7e7844d
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-17 23:34:32 +0800

    Edit about content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,30 +7,26 @@ const About = () => {
 		<div className={classes.container}>
 			<img className={classes.photo} src={img} alt="myself" />
 			<div className={classes.about}>
-				<h2>My story</h2>
+				<h2>I love programming and I love sharing it to others.</h2>
 				<p>
-					I felt like I was stuck in a place I needed to be freed from. Stuck
-					doing the same things and wasting my potential. I needed a change. A
-					career that will drive me to keep learning, to keep improving and to
-					become an asset I can be proud of. Having being stuck, doing the same
-					things everyday, with no sense of a future or learning, I knew I
-					needed a change.
+					My name is Jonathan and I am a self-taught developer. I have been
+					learning to code since beginning of 2019. Since then, I would spend
+					everyday learning and building web applications. I had always an
+					interest in programming, building games, private servers and websites
+					but not until last year, I took this passion seriously.
 				</p>
 				<p>
-					I did some self-reflections and asked professionals. I couldn’t really
-					find an answer. Until I looked back to whom I was before, perhaps
-					always. I found my passion.{" "}
-					<a href="https://92321.activeboard.com/">
-						From as young as 10, I was really into web development and
-						programming. I found websites and forums I started.
-					</a>{" "}
-					More than that, I remembered why I liked it, why I would wake up early
-					in the morning to spend some time in the family computer and code. I
-					started coding after seeing someone’s profile in a social networking
+					I started coding after seeing someone’s profile in a social networking
 					site (Friendster) having a different layout and design than everyone
 					else. It was completely abstract. I was amazed and I wanted to do the
 					same thing too. I got involved in the community and became a
-					moderator.
+					moderator. From as young as 10, I was already excited about web
+					development. Unfortunately, searching today the username I have been
+					using since the time I first made contact through the internet, I
+					could only find some websites and{" "}
+					<a href="https://92321.activeboard.com/">forums I built.</a> More than
+					that, I remembered why I liked it, why I would wake up early in the
+					morning to spend some time in the family computer and code.
 				</p>
 				<p>
 					Having moved here to Australia, I lost my passion. Maybe because we
