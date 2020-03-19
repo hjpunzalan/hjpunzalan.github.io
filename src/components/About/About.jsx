6fcd9edf44720af8b54abc19ea7a26bb97dea2a2
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-19 23:51:05 +0800

    About section content edited

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,24 +7,24 @@ const About = () => {
 		<div className={classes.container}>
 			<img className={classes.photo} src={img} alt="myself" />
 			<div className={classes.about}>
-				<h2>I love programming and I love sharing it to others.</h2>
+				<h2>Becoming a self-taught developer</h2>
 				<p>
 					My name is Jonathan and I am a self-taught developer. I have been
 					learning to code since beginning of 2019. Since then, I would spend
 					everyday learning and building web applications. I had always an
-					interest in programming, building games, private servers and websites
-					but not until last year, I took this passion seriously.
+					interest in programming, and creating websites but not until last
+					year, I took this passion seriously.
 				</p>
 				<p>
 					From as young as 10, I was already excited about web development. I
-					started coding after seeing someone’s profile in a social networking
-					site (Friendster) having a different layout and design than everyone
-					else. It was completely abstract. I was amazed and I wanted to do the
-					same thing too. I began learning through a bunch of view sourcing of
-					web pages and searching answers through the web. Eventually, I got
-					involved in the community and became a help moderator for multiple
-					friendster forums. Unfortunately, searching my work, I could only find
-					some websites and{" "}
+					started to code after I saw a profile on a social networking site
+					(Friendster) that was completely different from the norm, than
+					everyone else. It was completely abstract. I was amazed and I wanted
+					to do the same thing too. I began learning through a bunch of view
+					sourcing of web pages and searching answers through the web.
+					Eventually, I got involved in the community and became a help
+					moderator for multiple friendster forums. Unfortunately, searching my
+					work, I could only find some websites and{" "}
 					<a href="https://92321.activeboard.com/">forums I built.</a> More than
 					that, I remembered why I liked it, why I would wake up early in the
 					morning to spend some time in the family computer and code.
