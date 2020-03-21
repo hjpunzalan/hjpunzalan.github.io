4950e6fba65d8229725ed3f4ec22f3216dca2f77
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-21 23:18:29 +0800

    Title font of Projects  and layout changed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -39,6 +39,15 @@ export class Projects extends Component {
 
 				{/*/////////////////// BUDGETY////////////////// */}
 				<div className={classes.project}>
+					<h2>Budgety App</h2>
+					<p>
+						A budgeting app inspired by my role as treasurer of a local public
+						speaking club. The idea was based from the process how budget was
+						handled and communicated within the club. On top of applying new
+						skills including Typescript and D3.js, this application has also
+						broadened my experience and skills of using MongoDB's querying and
+						aggregation pipeline.
+					</p>
 					<video
 						ref={this.budgetyVideo}
 						controls={true}
@@ -49,41 +58,39 @@ export class Projects extends Component {
 						<source src={budgetymp4} type="video/mp4" />
 						<source src={budgetypng} type="image/png" />
 					</video>
-					<div className={classes.desc}>
-						<h2>Budgety App</h2>
-						<p>
-							A budgeting app inspired by my role as treasurer of a local public
-							speaking club. The idea was based from the process how budget was
-							handled and communicated within the club. On top of applying new
-							skills including Typescript and D3.js, this application has also
-							broadened my experience and skills of using MongoDB's querying and
-							aggregation pipeline.
-						</p>
-						<h3>Features:</h3>
-						<ul>
-							<li>User Authentication with JWT and Sendgrid</li>
-							<li>Complete Responsive design</li>
-							<li>Creating and editing budgets with custom categories</li>
-							<li>Adding and editing transactions</li>
-							<li>
-								Transactions listed in tabulated format based on date with
-								accumulating balance
-							</li>
-							<li>
-								Interactive graphs using D3.js for analysis of annual or monthly
-								data
-							</li>
-						</ul>
-						<h3>
-							Tech:
-							<span className={classes.tech}>
-								Typescript, React, MongoDB, Express, D3.js
-							</span>
-						</h3>
-					</div>
+					<h3>Features:</h3>
+					<ul>
+						<li>User Authentication with JWT and Sendgrid</li>
+						<li>Complete Responsive design</li>
+						<li>Creating and editing budgets with custom categories</li>
+						<li>Adding and editing transactions</li>
+						<li>
+							Transactions listed in tabulated format based on date with
+							accumulating balance
+						</li>
+						<li>
+							Interactive graphs using D3.js for analysis of annual or monthly
+							data
+						</li>
+					</ul>
+					<h3>
+						Tech:
+						<span className={classes.tech}>
+							Typescript, React, MongoDB, Express, D3.js
+						</span>
+					</h3>
 				</div>
 				{/* ////////////////////////Toastmaster ///////////////////////// */}
 				<div className={classes.project}>
+					<h2>Toastmasters discussion board</h2>
+					<p>
+						A budgeting app inspired by my role as treasurer of a local public
+						speaking club. The idea was based from the process how budget was
+						handled and communicated within the club. On top of applying new
+						skills including Typescript and D3.js, this application has also
+						broadened my experience and skills of using MongoDB's querying and
+						aggregation pipeline.
+					</p>
 					<video
 						ref={this.toastmasterVideo}
 						controls={true}
@@ -94,38 +101,27 @@ export class Projects extends Component {
 						<source src={toastmastermp4} type="video/mp4" />
 						<source src={toastmasterpng} type="image/png" />
 					</video>
-					<div className={classes.desc}>
-						<h2>Toastmasters discussion board</h2>
-						<p>
-							A budgeting app inspired by my role as treasurer of a local public
-							speaking club. The idea was based from the process how budget was
-							handled and communicated within the club. On top of applying new
-							skills including Typescript and D3.js, this application has also
-							broadened my experience and skills of using MongoDB's querying and
-							aggregation pipeline.
-						</p>
-						<h3>Features:</h3>
-						<ul>
-							<li>User Authentication with JWT and Sendgrid</li>
-							<li>Complete Responsive design</li>
-							<li>Creating and editing budgets with custom categories</li>
-							<li>Adding and editing transactions</li>
-							<li>
-								Transactions listed in tabulated format based on date with
-								accumulating balance
-							</li>
-							<li>
-								Interactive graphs using D3.js for analysis of annual or monthly
-								data
-							</li>
-						</ul>
-						<h3>
-							Tech:
-							<span className={classes.tech}>
-								Typescript, React, MongoDB, Express, D3.js
-							</span>
-						</h3>
-					</div>
+					<h3>Features:</h3>
+					<ul>
+						<li>User Authentication with JWT and Sendgrid</li>
+						<li>Complete Responsive design</li>
+						<li>Creating and editing budgets with custom categories</li>
+						<li>Adding and editing transactions</li>
+						<li>
+							Transactions listed in tabulated format based on date with
+							accumulating balance
+						</li>
+						<li>
+							Interactive graphs using D3.js for analysis of annual or monthly
+							data
+						</li>
+					</ul>
+					<h3>
+						Tech:
+						<span className={classes.tech}>
+							Typescript, React, MongoDB, Express, D3.js
+						</span>
+					</h3>
 				</div>
 			</div>
 		);
