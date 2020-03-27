f34cc3e4b15e81f0293ac046dfa7181a88cd6b0f
hjpunzalan <hj.punzalan@hotmail.com>
2020-03-27 22:12:14 +0800

    added skills section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import classes from "./About.module.scss";
 import img from "../../images/myself.jpg";
 
+// Logos
+import jsLogo from "../../images/logo/js.svg";
+import typescriptLogo from "../../images/logo/typescript.svg";
+import sassLogo from "../../images/logo/sass.svg";
+import reactLogo from "../../images/logo/react.svg";
+import nodejsLogo from "../../images/logo/nodejs.svg";
+import mongodbLogo from "../../images/logo/mongodb.svg";
+import expressLogo from "../../images/logo/express.svg";
+import jestLogo from "../../images/logo/jest.svg";
+import d3Logo from "../../images/logo/d3.svg";
+
 const About = () => {
 	return (
 		<div className={classes.container}>
@@ -25,18 +36,28 @@ const About = () => {
 					a bunch of view sourcing of web pages and searching answers through
 					the web. I got involved in the community, made websites and my own{" "}
 					<a href="https://92321.activeboard.com/">forum</a> (it's amazing to
-					see it still running) .
+					see one still running) .
 				</p>
 				<p>
 					Having moved here to Australia, I lost my passion. Maybe because we
 					didn’t have internet for very long, or maybe there was too much change
 					around me, I got distracted. I was lead to the traditional way of
-					studying maths and science to get that standard engineering degree,
-					which I did. You might be asking, don’t you want to become an
-					engineer? No. Not anymore. Programming is completely different. I
-					haven’t found anything that keeps me up at night and wakes me up
-					early. Excited to learn and try something new. It’s that simple.
+					studying maths and science to get an engineering degree. However,
+					programming is completely different. I haven’t found anything that
+					keeps me up at night and wakes me up early. Excited to learn and try
+					something new. It’s that simple.
 				</p>
+				<div className={classes.skills}>
+					<img src={jsLogo} alt="js logo" />
+					<img src={typescriptLogo} alt="typescript logo" />
+					<img src={sassLogo} alt="sass logo" />
+					<img src={reactLogo} alt="react logo" />
+					<img src={nodejsLogo} alt="nodejs logo" />
+					<img src={mongodbLogo} alt="mongodb logo" />
+					<img src={expressLogo} alt="express logo" />
+					<img src={jestLogo} alt="jest logo" />
+					<img src={d3Logo} alt="d3 logo" />
+				</div>
 			</div>
 		</div>
 	);
